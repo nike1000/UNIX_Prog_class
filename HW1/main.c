51feecc945d2a44c278d9cdd7fa4dd3a8493ff4b
nike1000 <st505102000@gmail.com>
2016-04-07 16:15:40 +0800

    My HW1

--- HW1/main.c
+++ HW1/main.c
@@ -3,14 +3,52 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/resource.h>
 
 extern void random_length_task();
 
-int main() {
+struct timeval tvdiff(struct timeval t1, struct timeval t0) {
+    t1.tv_sec -= t0.tv_sec;
+    if(t1.tv_usec >= t0.tv_usec) {
+        t1.tv_usec = t1.tv_usec - t0.tv_usec;
+    } else {
+        t1.tv_usec = t1.tv_usec + 1000000 - t0.tv_usec;
+        t1.tv_sec--;
+    }
+	
+	struct timeval diff;
+	diff.tv_sec=t1.tv_sec;
+	diff.tv_usec=t1.tv_usec;
+    return diff;
+}
+
+
+int main(int argc,char *argv[]) {
 	int n = 10;
+    if(argc == 2)
+    {
+        sscanf (argv[1],"%d",&n);
+    }
+
 	srand(time(0) ^ getpid());
-	while(n-- > 0) {
-		random_length_task();
+	while(1) {
+		struct timeval tv1;
+        gettimeofday(&tv1, NULL);
+		
+        struct timeval diff;
+        
+        if(n<=0)
+            break;
+		n--;
+
+        random_length_task();
+        tv1.tv_usec+=99900;
+
+        struct timeval tv2;
+        gettimeofday(&tv2, NULL);
+
+        diff=tvdiff(tv1,tv2);
+        select(0, NULL, NULL, NULL, &diff);
 	}
 	return 0;
 }

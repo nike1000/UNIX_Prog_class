a51da6c30ae0a81da5fd87ba4f6c424ec4c59a5f
nike1000 <st505102000@gmail.com>
2016-04-10 02:11:42 +0800

    Output inject message to file

--- HW2/inject.c
+++ HW2/inject.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 static int (*orig_open)(const char *pathname, int flags) = NULL;
 static FILE *(*orig_fopen)(const char *path, const char *mode) = NULL;
@@ -16,7 +17,8 @@ static int (*orig_rename)(const char *old, const char *new) = NULL;
 static int (*orig_bind)(int socket, const struct sockaddr *address, socklen_t address_len) = NULL;
 static int (*orig_connect)(int socket, const struct sockaddr *address, socklen_t address_len) = NULL;
 static int (*orig_getaddrinfo)(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res) = NULL;
-
+FILE *flogopen(const char *path, const char *mode);
+int flogclose(FILE *fp);
 
 int open(const char *pathname, int flags)
 {
@@ -32,8 +34,9 @@ int open(const char *pathname, int flags)
 
     if (orig_open != NULL) 
     {
-        /*fprintf(stderr, "***kshuang Open***\n");*/
-        printf("***open*** path:%s, flags:%d\n", pathname, flags);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***open*** path:%s, flags:%d\n", pathname, flags);
+        flogclose(fp);
         return orig_open(pathname, flags);
     }
 }
@@ -53,8 +56,9 @@ FILE *fopen(const char *path, const char *mode)
 
     if (orig_fopen != NULL) 
     {
-        /*fprintf(stderr, "***kshuang fopen***\n");*/
-        printf("***fopen*** path:%s, mode:%s\n", path, mode);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***fopen*** path:%s, mode:%s\n", path, mode);
+        flogclose(fp);
         return orig_fopen(path, mode);
     }
 }
@@ -73,8 +77,9 @@ ssize_t read(int fildes, void *buf, size_t nbyte)
 
     if (orig_read != NULL) 
     {
-        /*fprintf(stderr, "***kshuang Read***\n");*/
-        printf("***read*** file descriptor:%d, size:%zu\n", fildes, nbyte);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***read*** file descriptor:%d, size:%zu\n", fildes, nbyte);
+        flogclose(fp);
         return orig_read(fildes, buf, nbyte);
     }
 }
@@ -93,8 +98,9 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
 
     if (orig_fread != NULL)
     {
-        /*fprintf(stderr, "***kshuang fread***\n");*/
-        printf("***fread*** ptr:%s, size:%zu, nmemb:%zu\n", ptr, size, nmemb);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***fread*** ptr:%s, size:%zu, nmemb:%zu\n", ptr, size, nmemb);
+        flogclose(fp);
         return orig_fread(ptr, size, nmemb, stream);
     }
 }
@@ -113,8 +119,9 @@ size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 
     if (orig_fwrite != NULL)
     {
-        //fprintf(stderr, "***kshuang fwrite***\n");
-        printf("***fwrite*** size:%zu, nmemb:%zu\n", size, nmemb);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***fwrite*** size:%zu, nmemb:%zu\n", size, nmemb);
+        flogclose(fp);
         return orig_fwrite(ptr, size, nmemb, stream);
     }
 }
@@ -134,8 +141,9 @@ int close(int fildes)
 
     if (orig_close != NULL)
     {
-        /*fprintf(stderr, "***kshuang close***\n");*/
-        printf("***close*** file descriptor:%d\n", fildes);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***close*** file descriptor:%d\n", fildes);
+        flogclose(fp);
         return orig_close(fildes);
     }
 }
@@ -155,9 +163,10 @@ int fclose(FILE *fp)
 
     if (orig_fclose != NULL)
     {
-        /*fprintf(stderr, "***kshuang fclose***\n");*/
-        printf("***fclose***\n");
-         return orig_fclose(fp);
+        FILE *myfp = flogopen("0456085.log","a+");
+        fprintf(myfp, "***fclose***\n");
+        orig_fclose(myfp);
+        return orig_fclose(fp);
     }
 }
 
@@ -175,8 +184,9 @@ int fileno(FILE *stream)
 
     if (orig_fileno != NULL)
     {
-        /*fprintf(stderr, "***kshuang fileno***\n");*/
-        printf("***fileno***\n");
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***fileno***");
+        flogclose(fp);
         return orig_fileno(stream);
     }
 }
@@ -195,34 +205,34 @@ int rename(const char *old, const char *new)
 
     if (orig_rename != NULL)
     {
-        printf("***rename*** from %s to %s\n", old, new);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***rename*** from %s to %s\n", old, new);
+        flogclose(fp);
         return orig_rename(old, new);
     }
 }
 
 
-int bind(int socket, const struct sockaddr *address, socklen_t address_len)
-{
-    if (orig_bind == NULL)
-    {
-        void *handle = dlopen("libc.so.6", RTLD_LAZY);
+/*int bind(int socket, const struct sockaddr *address, socklen_t address_len)*/
+/*{*/
+    /*if (orig_bind == NULL)*/
+    /*{*/
+        /*void *handle = dlopen("libc.so.6", RTLD_LAZY);*/
         
-        if (handle != NULL)
-        {
-            orig_bind = dlsym(handle, "bind");
-        }
-    }
+        /*if (handle != NULL)*/
+        /*{*/
+            /*orig_bind = dlsym(handle, "bind");*/
+        /*}*/
+    /*}*/
 
-    if (orig_bind != NULL)
-    {
-        struct sockaddr_in *saddr_in = (struct sockaddr_in *)address;
+    /*if (orig_bind != NULL)*/
+    /*{*/
+        /*struct sockaddr_in *saddr_in = (struct sockaddr_in *)address;*/
 
-        printf("***bind*** %s------------------\n",inet_ntoa(saddr_in->sin_addr));
-        return orig_bind(socket, address, address_len);
-    }
-}
-
-
+        /*printf("***bind*** %s------------------\n",inet_ntoa(saddr_in->sin_addr));*/
+        /*return orig_bind(socket, address, address_len);*/
+    /*}*/
+/*}*/
 
 int connect(int socket, const struct sockaddr *address, socklen_t address_len)
 {
@@ -239,13 +249,13 @@ int connect(int socket, const struct sockaddr *address, socklen_t address_len)
     if (orig_connect != NULL)
     {
         struct sockaddr_in *saddr_in = (struct sockaddr_in *)address;
-
-        printf("***connect*** %s------------------\n",inet_ntoa(saddr_in->sin_addr));
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***connect*** %s\n",inet_ntoa(saddr_in->sin_addr));
+        flogclose(fp);
         return orig_connect(socket, address, address_len);
     }
     
 }
-
 
 int getaddrinfo(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res)
 {
@@ -261,42 +271,45 @@ int getaddrinfo(const char *node, const char *service, const struct addrinfo *hi
 
     if (orig_getaddrinfo != NULL)
     {
-        printf("***getaddrinfo*** host:%s, service:%s------------------\n",node, service);
+        FILE *fp = flogopen("0456085.log","a+");
+        fprintf(fp, "***getaddrinfo*** host:%s, service:%s\n",node, service);
+        flogclose(fp);
         return orig_getaddrinfo(node, service, hints, res);
     }
 }
 
+FILE *flogopen(const char *path, const char *mode)
+{
+    if (orig_fopen == NULL)
+    {
+        void *handle = dlopen("libc.so.6", RTLD_LAZY);
+        
+        if (handle != NULL)
+        {
+            orig_fopen = dlsym(handle, "fopen");
+        }
+    }
 
+    if (orig_fopen != NULL)
+    {
+        return orig_fopen(path, mode);
+    }
+}
 
+int flogclose(FILE *fp)
+{
+    if (orig_fclose == NULL)
+    {
+        void *handle = dlopen("libc.so.6", RTLD_LAZY);
+        
+        if (handle != NULL)
+        {
+            orig_fclose = dlsym(handle, "fclose");
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (orig_fclose != NULL)
+    {
+        return orig_fclose(fp);
+    }
+}
